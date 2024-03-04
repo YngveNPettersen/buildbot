bcbf33c6869dade565e2d730f3f97c36f0677d38
Yngve N. Pettersen <yngve@vivaldi.com>
2024-03-04 20:53:34 +0100

    fixup! Close connection immediately during KeepAlive if it is not associated with a worker (#4678)
    This is a just-in-case-logic is the connection is somehow detached from all Builders, but still leaving the connection active

--- master/buildbot/worker/protocols/pb.py
+++ master/buildbot/worker/protocols/pb.py
@@ -148,7 +148,7 @@ class Connection(base.Connection, pb.Avatar):
 
     def _do_keepalive(self):
         if not self.worker or (isinstance(self.worker, AbstractWorker) and
-                               self. self.worker.isConnected() != self):
+                               self.worker.isConnected() != self):
             self.loseConnection()
             self.notifyDisconnected()
             return defer.fail(None)
